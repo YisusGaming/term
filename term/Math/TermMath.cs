267889c50f6e03ac6f795bfba5dceb9307308f12
Yisus <jesusgameroom@gmail.com>
2023-08-09 11:20:32 -0400

    arithmetic addition

--- term/Math/TermMath.cs
+++ term/Math/TermMath.cs
@@ -18,11 +18,26 @@ public static class TermMaths
         INVALID
     }
 
-    public static void RunArithmetics(string property)
+    public static float? RunArithmetics(string property)
     {
         if (property.Contains((char) ArithmeticOperators.ADDITION))
         {
-            Console.WriteLine(property);
+            string[] addition = property.Split((char) ArithmeticOperators.ADDITION);
+            try
+            {
+                float x1 = Convert.ToSingle(addition[0].Trim());
+                float x2 = Convert.ToSingle(addition[1].Trim());
+                
+                return x1 + x2;
+            }
+            catch (Exception exception)
+            {
+                throw new ArithmeticFaultException(exception.Message, exception);
+            }
         }
+
+        // If no arithmetic operation is found,
+        // return null.
+        return null;
     }
 }
